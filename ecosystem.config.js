2a058d4caf8454510b95f51a78070b22c2f6afda
JewelHFahim <jewelhfahim@gmail.com>
2026-03-05 02:53:59 +0600

    security patch updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,9 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 5000,
-        MONGO_URI: 'mongodb://127.0.0.1:27017/mern-ecommerce'
+        MONGO_URI: 'mongodb://127.0.0.1:27017/mern-ecommerce',
+        BACKEND_URL: 'https://api.thefirecutter.store',
+        STORAGE_TYPE: 'LOCAL'
       }
     },
     {
@@ -19,8 +21,8 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3001,
-        NEXT_PUBLIC_API_URL: 'http://176.57.189.196:5000/api/v1',
-        NEXT_PUBLIC_STOREFRONT_URL: 'http://176.57.189.196:3000'
+        NEXT_PUBLIC_API_URL: 'https://api.thefirecutter.store/api/v1',
+        NEXT_PUBLIC_STOREFRONT_URL: 'https://thefirecutter.store'
       }
     },
     {
@@ -31,7 +33,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        NEXT_PUBLIC_API_URL: 'http://176.57.189.196:5000/api/v1'
+        NEXT_PUBLIC_API_URL: 'https://api.thefirecutter.store/api/v1'
       }
     }
   ]
